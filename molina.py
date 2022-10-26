70cd9bcd4660f36950eeb68e0008e0376facff29
“kamesh20” <kamesh20k@gmail.com>
2022-10-26 02:31:25 +0000

    3rd

--- molina.py
+++ molina.py
@@ -4,7 +4,7 @@ a = float(input('Enter a: '))
 b = float(input('Enter b: '))  
 c = float(input('Enter c: '))  
   
-# calculate the discriminant  
+# calculate the discriminants:  
 d = (b**2) - (4*a*c)  
   
 # find two solutions  
