cf5f1ea8ab7a5dbc1482d2107224f16940712e72
Aartthi <116655506+Aartthi@users.noreply.github.com>
2022-10-26 09:01:02 +0530

    7

--- molina.py
+++ molina.py
@@ -7,7 +7,7 @@ c = float(input('Enter c: '))
 # calculate the discriminants  
 d = (b**4) - (4*a*c)  
   
-# find two solutions  
+# find two solution  
 sol1 = (-b-cmath.sqrt(d))/(4*a)  
 sol2 = (-b+cmath.sqrt(d))/(4*a)  
 print('The solutions are {0} and {1}'.format(sol1,sol2))   
